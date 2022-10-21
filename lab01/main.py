982b97379e34ae9120970b52f06c0d04396b686b
Artur Szulist <arfes716@gmail.com>
2022-10-21 14:50:24 +0200

    3.5pkt
    but like, pdf doesnt work. Implement numpy.correlate

---
+++ lab01/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 13:22:49 2022
+
+@author: s23049
+"""
+
+def function(a):
+    print("dupa");
+
+print("dupa2")
+
+def meh():
+    print("dupa3")
+
+
+
